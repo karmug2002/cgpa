bf190d9a8cc0c7c6b7c350a1c2aa89d87a87f79e
karmug2002 <karmug4614648@gmail.com>
2021-11-27 18:47:46 +0530

    second commit

--- CGPa/src/basepackage/InputProcessor.java
+++ CGPa/src/basepackage/InputProcessor.java
@@ -2,20 +2,25 @@ package basepackage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class InputProcessor
 {
 
 	private HashMap<String,Float> semInfo;
+	private Scanner scanner;
+	private float in;
 	public InputProcessor(HashMap<String,Float> semInfo)
 	{
 		this.semInfo=semInfo;
+		scanner=new Scanner(System.in);
 	}
 	public ArrayList<Float> getInput()
 	{
 		ArrayList<Float> input= new ArrayList<Float>();
 		for(String s : semInfo.keySet())
 		{
+			in=scanner.nextFloat();
 			print(s);
 		}
 		return input;
